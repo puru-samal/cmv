a4ed4fe4d344a0bc927e8657403301240a4174ca
Puru Samal <puruboii@gmail.com>
2025-10-14 17:05:58 -0500

    minor ...

--- src/setup/LibriTTS_extract.py
+++ src/setup/LibriTTS_extract.py
@@ -1,25 +1,33 @@
-from collections import defaultdict
 from pathlib import Path
 import multiprocessing
 import tarfile
 import os
 import time
-from typing import List
-import argparse
+import hydra
+from omegaconf import DictConfig
 
-def extract_targz_file(targz_path: str) -> None:
+def extract_targz_file(targz_path: str, extract_dir: str) -> None:
         '''A synchronous helper function to extract one or more tar.gz files.'''
         if tarfile.is_tarfile(targz_path):
             with tarfile.open(targz_path, 'r:gz') as tar:
-                tar.extractall(path=os.path.dirname(targz_path), filter="fully_trusted")
-            print(f"[OK] Extracted {targz_path}")
+                tar.extractall(path=extract_dir, filter="fully_trusted")
+            print(f"[OK] Extracted {targz_path} to {extract_dir}")
             os.remove(targz_path)
             print(f"[OK] Removed {targz_path}")
         else:
             print(f"[ERROR] {targz_path} is not a valid tar file.")
 
-def parallel_extract(targz_paths: List[str]) -> None:
+@hydra.main(version_base=None, config_path="../../configs/setup", config_name="LibriTTS_extract")
+def parallel_extract(cfg: DictConfig) -> None:
     '''Extract tar.gz files in parallel using multiprocessing.'''
+    # Config parameters
+    data_dir = cfg.data_dir
+    extract_dir = cfg.extract_dir
+    extension = cfg.extension
+
+    # Find all .tar.gz files in the data directory
+    targz_paths = list(Path(data_dir).rglob(f"*{extension}"))
+    extract_dir = os.path.abspath(extract_dir)
     num_file, num_cpus = len(targz_paths), min(multiprocessing.cpu_count(), len(targz_paths))
     if num_file == 0:
         print("No .tar.gz files found for extraction.")
@@ -30,10 +38,10 @@ def parallel_extract(targz_paths: List[str]) -> None:
     print(f"\t- Number of files: {num_file}")
     print(f"\t- Number of CPU cores: {num_cpus}")
     with multiprocessing.Pool(processes=num_cpus) as pool:
-        pool.map(extract_targz_file, targz_paths)
+        pool.starmap(extract_targz_file, [(path, extract_dir) for path in targz_paths])
     elapsed = time.perf_counter() - start_time
     print(f"Extraction completed: Took {elapsed:.2f} seconds.")
 
 
 if __name__ == "__main__":
-    pass
+    parallel_extract()
